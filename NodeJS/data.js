58b333b501efacd93612fac902332664b834f735
Abishek V H <abishekvh@gmail.com>
2026-02-28 18:47:07 +0530

    Added NodeJS lessons - Global, Modules, FS

---
+++ NodeJS/data.js
@@ -0,0 +1,4 @@
+names = ['Abishek', 'Raghul', 'Ragul', 'Praveen Kumar'];
+schools = ['TVS', 'CEOA', 'KVS', 'SBOA'];
+
+module.exports = {studentName : names, schools}; //Property : Value (or) simply Value
