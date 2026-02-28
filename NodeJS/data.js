d39125b1bcd586545e41ce9eb28396cf0502b47f
Abishek V H <abishekvh@gmail.com>
2026-02-28 18:49:19 +0530

    Revert "Added NodeJS lessons - Global, Modules, FS"
    This reverts commit 58b333b501efacd93612fac902332664b834f735.

--- NodeJS/data.js
+++
@@ -1,4 +0,0 @@
-names = ['Abishek', 'Raghul', 'Ragul', 'Praveen Kumar'];
-schools = ['TVS', 'CEOA', 'KVS', 'SBOA'];
-
-module.exports = {studentName : names, schools}; //Property : Value (or) simply Value
